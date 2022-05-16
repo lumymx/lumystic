6a2745b6dfa2aca1113be77caadb37f2e70c092d
lumymx <lumy3uwu@gmail.com>
2022-05-16 18:37:06 +0300

    new classes: input and actions

--- src/main.js
+++ src/main.js
@@ -22,21 +22,6 @@ class Canvas {
     let physics = new Physics();
     this.g_result = physics.g_result;
   }
-  get mspos() {
-    return createVector(mouseX, mouseY);
-  }
-  get mspos_gr() {
-    let msx = mouseX;
-    let msy = mouseY;
-    if (chbox.checked) {
-      return createVector(
-        round(msx / _grid) * _grid,
-        round(msy / _grid) * _grid
-      );
-    } else {
-      return createVector(msx, msy);
-    }
-  }
   setup() {
     var canvas = createCanvas(width, height);
     canvas.parent("canvas_block");
@@ -51,17 +36,22 @@ class Canvas {
     for (let i = 0; i <= int(height / _grid); i++) {
       line(0, i * _grid, width, i * _grid);
     }
-    noStroke();
+    let actions = new Actions();
     switch (this.state) {
+      case 0:
+        actions.drag();
+        break;
       case 2:
       case 3:
-        this.new_dot();
+        actions.new_dot();
+        break;
     }
     for (let dot of this.dots) {
       dot.show();
       if (dot instanceof Node) {
         dot.move();
         dot.walls();
+        dot.collide();
       }
     }
   }
@@ -85,39 +75,11 @@ class Canvas {
         document.getElementById("node").style.background = "white";
     }
   }
-  new_dot() {
-    if (mouseButton == LEFT && mouseIsPressed == true) {
-      let dot_near = false;
-      let canvas = new Canvas();
-      let mspos_gr = canvas.mspos_gr;
-      for (let i = 0; i < this.dots.length; i++) {
-        let vec = p5.Vector.sub(mspos_gr, this.dots[i].pos);
-        let dist = vec.mag();
-        if (dist < this.dsize + 2.5) dot_near = true;
-      }
-      if (dot_near == false) {
-        switch (this.state) {
-          case 2:
-            this.dots.push(new Pin(mspos_gr));
-            break;
-          case 3:
-            this.dots.push(
-              new Node(
-                mspos_gr,
-                createVector(0, 0),
-                createVector(0, this.g_result)
-              )
-            );
-            break;
-        }
-      }
-    }
-  }
 }
 
 class Physics {
   _g = 9.8066;
-  _energy = 0.9;
+  _energy = 0.7;
   constructor() {
     if (Physics._instance) {
       return Physics._instance;
@@ -130,6 +92,89 @@ class Physics {
   }
   get energy() {
     return this._energy;
+  }
+}
+
+class Input {
+  constructor() {
+    if (Input._instance) {
+      return Input._instance;
+    }
+    Input._instance = this;
+  }
+  get mspos() {
+    return createVector(mouseX, mouseY);
+  }
+  get mspos_gr() {
+    let msx = mouseX;
+    let msy = mouseY;
+    if (chbox.checked) {
+      return createVector(
+        round(msx / _grid) * _grid,
+        round(msy / _grid) * _grid
+      );
+    } else {
+      return createVector(msx, msy);
+    }
+  }
+}
+
+class Actions {
+  constructor() {
+    if (Actions._instance) {
+      return Actions._instance;
+    }
+    Actions._instance = this;
+    canvas = new Canvas();
+  }
+  drag() {
+    let input = new Input();
+    let mspos_gr = input.mspos_gr;
+    let x = -1;
+    for (let i = 0; i < canvas.dots.length; i++) {
+      let vec = p5.Vector.sub(mspos_gr, canvas.dots[i].pos);
+      let dist = vec.mag();
+      if (dist < canvas.dsize) {
+        x = i;
+        break;
+      }
+    }
+    if (mouseButton == LEFT && mouseIsPressed == true && x != -1) {
+      let mspos = input.mspos;
+      canvas.dots[x].pos = mspos;
+      canvas.dots[x].vel = createVector(0, 0);
+    }
+  }
+  new_dot() {
+    let input = new Input();
+    if (mouseButton == LEFT && mouseIsPressed == true) {
+      let dot_near = false;
+      let mspos_gr = input.mspos_gr;
+      for (let i = 0; i < canvas.dots.length; i++) {
+        let vec = p5.Vector.sub(mspos_gr, canvas.dots[i].pos);
+        let dist = vec.mag();
+        if (dist < canvas.dsize) {
+          dot_near = true;
+          break;
+        }
+      }
+      if (dot_near == false) {
+        switch (canvas.state) {
+          case 2:
+            canvas.dots.push(new Pin(mspos_gr));
+            break;
+          case 3:
+            canvas.dots.push(
+              new Node(
+                mspos_gr,
+                createVector(0, 0),
+                createVector(0, canvas.g_result)
+              )
+            );
+            break;
+        }
+      }
+    }
   }
 }
 
@@ -150,8 +195,9 @@ class Node extends Dot {
     this.energy = physics.energy;
   }
   show() {
-    let canvas = new Canvas();
-    let mspos = canvas.mspos;
+    noStroke();
+    let input = new Input();
+    let mspos = input.mspos;
     let vec = p5.Vector.sub(mspos, this.pos);
     let dist = vec.mag();
     if (dist > this.dsize / 2) {
@@ -167,6 +213,8 @@ class Node extends Dot {
     this.pos.add(this.vel);
     if (this.pos.y > ceil(height - this.dsize / 2)) {
       this.pos.y = ceil(height - this.dsize / 2);
+    } else if (this.pos.y < ceil(this.dsize / 2)) {
+      this.pos.y = ceil(this.dsize / 2);
     }
     if (this.pos.x > ceil(width - this.dsize / 2)) {
       this.pos.x = ceil(width - this.dsize / 2);
@@ -182,6 +230,30 @@ class Node extends Dot {
       this.vel.y *= -1 * this.energy;
     }
   }
+  collide() {
+    let dir;
+    let dist;
+    let v1;
+    let v2;
+    let canvas = new Canvas();
+    for (let i = 0; i < canvas.dots.length; i++) {
+      dir = p5.Vector.sub(canvas.dots[i].pos, this.pos);
+      dist = dir.mag();
+      if (dist <= this.dsize) {
+        dir.normalize();
+        let correction = this.dsize - dist;
+        v1 = p5.Vector.dot(dir, this.vel);
+        v2 = p5.Vector.dot(dir, canvas.dots[i].vel);
+        dir.mult(v1 - v2, this.energy);
+        this.pos.sub(p5.Vector.mult(dir, correction / 2));
+        this.vel.sub(dir);
+        if (canvas.dots[i] instanceof Node) {
+          canvas.dots[i].pos.add(p5.Vector.mult(dir, correction / 2));
+          canvas.dots[i].vel.add(dir);
+        }
+      }
+    }
+  }
 }
 
 class Pin extends Dot {
@@ -189,8 +261,9 @@ class Pin extends Dot {
     super(pos, dsize);
   }
   show() {
-    let canvas = new Canvas();
-    let mspos = canvas.mspos;
+    noStroke();
+    let input = new Input();
+    let mspos = input.mspos;
     let vec = p5.Vector.sub(mspos, this.pos);
     let dist = vec.mag();
     if (dist > this.dsize / 2) {
